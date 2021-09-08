df5d4d4eb7041c32860ebe644ffaec7c35e810a7
方明坤 <fangmingkun.cosmo@haier.com>
2021-09-08 17:44:22 +0800

    add Hello

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @Author: 方明坤
+ * @Date: 2021/09/08/17:38
+ * @Description:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
